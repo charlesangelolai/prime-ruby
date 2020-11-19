50d27501a4ac5dfcc13dfc557a344fcdbc191591
Charles <charlesangeloplai@gmail.com>
2020-11-19 16:44:19 -0500

    commit tree e50a2312560c125e799f9bc6ae76d56b00d490b3

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
 # Add  code here!
+def prime?(num)
+    flag = false
+    if num <= 1
+        return flag
+    else
+        (2..num-1).none? do |divisor|
+            num % divisor == 0
+        end
+    end
+end
